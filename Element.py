f2ca88a5650169d56f2f501c6cdb063366d591e3
Maitreya Venkataswamy <mvenkataswamy3@gatech.edu>
2020-05-08 11:18:10 -0400

    Initialized Element and Face classes

---
+++ Element.py
@@ -0,0 +1,7 @@
+"""
+Copyright (C) 2020 Maitreya Venkataswamy - All Rights Reserved
+"""
+
+class Element:
+    def __init__(self):
+        pass
---
+++ Face.py
@@ -0,0 +1,9 @@
+"""
+Copyright (C) 2020 Maitreya Venkataswamy - All Rights Reserved
+"""
+
+from Element import Element
+
+class Face(Element):
+    def __init__(self):
+        super().__init__()
