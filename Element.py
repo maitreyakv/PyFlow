9b7a199aec89c76873177821c3d8839b9a5a6f97
Maitreya Venkataswamy <mvenkataswamy3@gatech.edu>
2020-05-08 13:16:14 -0400

    Initialized Element class

--- Element.py
+++
@@ -1,7 +0,0 @@
-"""
-Copyright (C) 2020 Maitreya Venkataswamy - All Rights Reserved
-"""
-
-class Element:
-    def __init__(self):
-        pass
---
+++ solver/Element.py
@@ -0,0 +1,5 @@
+class Element:
+
+    # Constructor for Element
+    def __init__(self):
+        pass
