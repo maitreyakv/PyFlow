b609a9302d6dba887f2fbf14b79b9224abfdacde
Maitreya Venkataswamy <mvenkataswamy3@gatech.edu>
2020-05-14 16:22:31 -0400

    Updated testing driver program

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
 from tqdm import tqdm
 
-from solver.Simulation import Simulation
 from solver.GhostCell import GhostCell
 from solver.BoundaryFace import BoundaryFace
 
@@ -10,6 +9,11 @@ def main():
     from solver.GmshGridReader import GmshGridReader
     mesh_reader = GmshGridReader()
     nodes, faces, cells = mesh_reader.read_grid_from_file("/Users/maitreya/Desktop/pipe-3d-mesh/pipe.msh")
+
+    interior_cells = [cell for cell in cells if not isinstance(cell, GhostCell)]
+    ghost_cells = [cell for cell in cells if isinstance(cell, GhostCell)]
+
+    boundary_faces = [face for face in faces if isinstance(face, BoundaryFace)]
 
     # Initial conditions
     print("initializing flow field...")
@@ -25,26 +29,26 @@ def main():
         E = p / (0.4 * rho) + 0.5 * u**2
         cell.flow.W_ = rho * np.array([1., u, 0., 0., E])
 
-    # Construct the simulation
-    sim = Simulation(nodes, faces, cells)
-
     # Initialize Thermo
     from solver.CPGThermo import CPGThermo
     thermo = CPGThermo(1.4, 0.72, 287.058)
 
-    # Apply BCs
-    for face in faces:
-        if isinstance(face, BoundaryFace):
-            face.apply_bc_to_ghost_cell()
+    # Update all varaibles in interior cells
+    for interior_cell in interior_cells:
+        interior_cell.flow.update(thermo)
 
-    # Update all variables in all cells
-    for cell in cells:
-        cell.flow.update(thermo)
+    # Apply BCs to GhostCells and BoundaryFaces
+    for boundary_face in boundary_faces:
+        boundary_face.apply_bc(thermo)
+
+    # Update all variables in GhostCells
+    for ghost_cell in ghost_cells:
+        ghost_cell.flow.update(thermo)
 
     # Write data file
     from solver.VTKWriter import VTKWriter
     mesh_writer = VTKWriter()
-    write_ghost = False
+    write_ghost = True
     mesh_writer.write_to_file(nodes, cells, "/Users/maitreya/Desktop/pipe-3d-mesh/pipe.vtk", write_ghost=write_ghost)
 
 if __name__ == "__main__":
