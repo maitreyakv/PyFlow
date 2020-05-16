Fixed bug in Face area calculation and changed Cell volume calculation method

solver/Face.py
@@ -23,7 +23,8 @@ class Face(Element):
         self.n_ /= np.linalg.norm(self.n_)
 
         # Compute the surface area of the Face
-        self.area = abs(0.5 * self.n_.dot(sum([fast_cross(nodes[i].r_, nodes[i+1].r_) for i in range(self.size-1)])))
+        self.area = 0.5 * np.linalg.norm(fast_cross(nodes[2].r_ - nodes[0].r_, nodes[2].r_ - nodes[1].r_))
+        #self.area = abs(0.5 * self.n_.dot(sum([fast_cross(nodes[i].r_, nodes[i+1].r_) for i in range(self.size-1)])))
 
         # Initialize the left and right cells to None
         self.left_cell = None

solver/TetrahedronCell.py
@@ -16,8 +16,4 @@ class TetrahedronCell(Cell):
 
     def compute_volume(self):
         # Compute the volume
-        a = self.nodes[0].r_
-        b = self.nodes[1].r_
-        c = self.nodes[2].r_
-        d = self.nodes[3].r_
-        return abs(np.dot(a - d, fast_cross(b - d, c - d))) / 6.0
+        return sum([face.r_.dot(face.normal(self.r_) * face.area) for face in self.faces]) / 3.

test/test_Face.py
@@ -42,6 +42,7 @@ class TestFace(unittest.TestCase):
     c2 = TetrahedronCell(faces_2, nodes_2)
 
     f10 = Face(n2, n12, n3)
+    f11 = Face(n1, n2, n4)
 
     def test_n_(self):
         self.assertTrue(np.allclose(self.f1.n_, 1.0 / np.sqrt(3.0) * np.array([1., 1., 1.])))
@@ -53,7 +54,7 @@ class TestFace(unittest.TestCase):
         self.assertTrue(np.allclose(self.f1.normal(np.array([4., 4., 4.])), -1.0 / np.sqrt(3.0) * np.array([1., 1., 1.])))
 
     def test_area(self):
-        self.assertEqual(self.f2.area, 6.)
+        self.assertEqual(self.f11.area, 2.)
 
     def test_r_(self):
         self.assertTrue(np.allclose(self.f2.r_, np.array([1., 1.5, 0.])))