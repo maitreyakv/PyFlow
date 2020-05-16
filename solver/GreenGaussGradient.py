3de47974932ef2eaa9edf4ebde4aee7eebbf9446
Maitreya Venkataswamy <mvenkataswamy3@gatech.edu>
2020-05-16 16:33:36 -0400

    Notation change in GreenGaussGradien

--- solver/GreenGaussGradient.py
+++ solver/GreenGaussGradient.py
@@ -13,20 +13,20 @@ class GreenGaussGradient(Gradient):
     # Implements computation of the velocity and temperature gradients
     def compute_gradients(self, cell):
         # Initialize Cell gradients to zero
-        cell.flow.grad_u = np.zeros(3)
-        cell.flow.grad_v = np.zeros(3)
-        cell.flow.grad_w = np.zeros(3)
-        cell.flow.grad_T = np.zeros(3)
+        cell.flow.grad_u_ = np.zeros(3)
+        cell.flow.grad_v_ = np.zeros(3)
+        cell.flow.grad_w_ = np.zeros(3)
+        cell.flow.grad_T_ = np.zeros(3)
 
         # Compute gradients by looping over the Faces of the Cell
         for face in cell.faces:
-            cell.flow.grad_u += face.flow.v_[0] * face.normal(cell.r_) * face.area
-            cell.flow.grad_v += face.flow.v_[1] * face.normal(cell.r_) * face.area
-            cell.flow.grad_w += face.flow.v_[2] * face.normal(cell.r_) * face.area
-            cell.flow.grad_T += face.flow.T * face.normal(cell.r_) * face.area
+            cell.flow.grad_u_ += face.flow.v_[0] * face.normal(cell.r_) * face.area
+            cell.flow.grad_v_ += face.flow.v_[1] * face.normal(cell.r_) * face.area
+            cell.flow.grad_w_ += face.flow.v_[2] * face.normal(cell.r_) * face.area
+            cell.flow.grad_T_ += face.flow.T * face.normal(cell.r_) * face.area
 
         # Normalize by the cell volume to finish gradient computation
-        cell.flow.grad_u /= cell.vol
-        cell.flow.grad_v /= cell.vol
-        cell.flow.grad_w /= cell.vol
-        cell.flow.grad_T /= cell.vol
+        cell.flow.grad_u_ /= cell.vol
+        cell.flow.grad_v_ /= cell.vol
+        cell.flow.grad_w_ /= cell.vol
+        cell.flow.grad_T_ /= cell.vol
