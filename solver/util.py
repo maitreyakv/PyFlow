51a2aec6471e38171bd7a05fb05771f9510cffb8
Maitreya Venkataswamy <mvenkataswamy3@gatech.edu>
2020-05-17 10:48:31 -0400

    Used Numba to compile cross product function

--- solver/util.py
+++ solver/util.py
@@ -1,5 +1,7 @@
 import numpy as np
+from numba import jit
 
+@jit(nopython=True)
 def fast_cross(v1, v2):
     return np.array([v1[1]*v2[2] - v1[2]*v2[1],
                      v1[2]*v2[0] - v1[0]*v2[2],
