502eb8e4b3b611ddc07469775f9869ef725b1651
Maitreya Venkataswamy <mvenkataswamy3@gatech.edu>
2020-05-18 15:05:40 -0400

    Removed all classes in preparation for complete rewrite with no classes

--- solver/GhostCell.py
+++
@@ -1,9 +0,0 @@
-from solver.Cell import Cell
-
-# TODO: Add doc for class
-class GhostCell(Cell):
-
-    # Constructor for GhostCell
-    def __init__(self, faces, nodes):
-        # Call to superclass constructor
-        super().__init__(faces, nodes)
