42b6f35d81687f0c6ca4ed50fe1617cd27851bc1
Maitreya Venkataswamy <mvenkataswamy3@gatech.edu>
2020-05-19 01:34:28 -0400

    Replaced util.py

---
+++ solver/util.py
@@ -0,0 +1,8 @@
+from numpy import array
+from numba import jit
+
+@jit(nopython=True)
+def fast_cross(v1, v2):
+    return array([v1[1]*v2[2] - v1[2]*v2[1],
+                  v1[2]*v2[0] - v1[0]*v2[2],
+                  v1[0]*v2[1] - v1[1]*v2[0]])
